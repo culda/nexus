68c6701989f9fdbe295251a256b76161e956bcb4
Bogdan Culda <culda@proton.me>
2023-05-14 14:25:05 +0100

    readme

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 # Nexus - defi like a boss
 
-## 1INCH API codegen
+A CLI tool for buying/selling tokens on EVM chains using the 1INCH Aggregator protocol.
+Warning: This is an untested tool, bad things can happen. Always use small amounts to test first
 
-`oapigen generate --package-name inch_api -i inch-api/v5-swagger.json -g rust -o inch-api`
+Future plans:
+
+- airdrop farming. i.e use destReceiver from 1INCH to receive swap tokens directly into wallets generated from mnemonic
+- more advanced order types like TWAP for larger positions
+- volume/price alerts and automatic execution
+- whatever else I think about
+
+## Installation
+
+1. Create an `.env` file using `.env.template` as an example.
+
+2. Install rustup
+
+`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`
+
+3. Build binary
+
+`cargo build --release`
+
+4. Add binary to PATH and enjoy
+
+Assuming you cloned the repo at `~/nexus`, add the following to `.bashrc` or `.zshrc` depending on OS
+`export PATH=$PATH:~/nexus/target/release`
+
+### Example commands
+
+Swap 200 ARB into ETH on arbitrum
+`nexus sell -t 0x912CE59144191C1204E64559FE8253a0e49E6548 -a 200 -c arbitrum --allowmax`
+
+Swap 1 ETH into ARB on arbitrum
+`nexus buy -a 1 -t 0x912ce59144191c1204e64559fe8253a0e49e6548 -c arbitrum --allowmax`
+
+Swap 1 WETH for MUTE on zksync
+`nexus buy -a 1 -t 0xa49d7499271ae71cd8ab9ac515e6694c755d400c -c zksync --allowmax -n false`
+
+### Tips
+
+If you need to modify the API swagger file, use this to generate new bindings:
+
+`openapi-generator-cli generate --package-name inch_api -i inch-api/v5-swagger.json -g rust -o inch-api`
--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,3 +1,6 @@
+use dotenv::dotenv;
+use std::env;
+
 use ethers::{
     prelude::{
         k256::{self},
@@ -15,11 +18,24 @@ pub struct NexusClient {
 
 impl NexusClient {
     pub async fn new(chain: Chain) -> Self {
+        dotenv().ok();
         let rpc_url = match chain {
-            Chain::Mainnet => dotenv!("MAINNET_RPC"),
-            Chain::Goerli => dotenv!("GOERLI_RPC"),
-            Chain::ZkSync => "https://mainnet.era.zksync.io",
-            Chain::Arbitrum => "https://arb1.arbitrum.io/rpc",
+            Chain::Mainnet => match env::var("MAINNET_RPC") {
+                Ok(val) => val,
+                Err(_) => "https://eth.llamarpc.com".to_string(),
+            },
+            Chain::Goerli => match env::var("GOERLI_RPC") {
+                Ok(val) => val,
+                Err(_) => "https://goerli.blockpi.network/v1/rpc/public	".to_string(),
+            },
+            Chain::ZkSync => match env::var("ZKSYNC_RPC") {
+                Ok(val) => val,
+                Err(_) => "https://mainnet.era.zksync.io".to_string(),
+            },
+            Chain::Arbitrum => match env::var("ARBITRUM_RPC") {
+                Ok(val) => val,
+                Err(_) => "https://arb1.arbitrum.io/rpc".to_string(),
+            },
             _ => {
                 error!("Unsupported chain: {}", chain);
                 std::process::exit(1);
@@ -35,7 +51,7 @@ impl NexusClient {
 
         let wallet = builder.build().unwrap().with_chain_id(chain);
 
-        let mut provider = Provider::connect(rpc_url).await;
+        let mut provider = Provider::connect(rpc_url.as_str()).await;
         provider.set_chain(chain);
 
         let signer = SignerMiddleware::new(provider.clone(), wallet.clone());
